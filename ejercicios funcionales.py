8e23e4e7da47d3b6a7218ac5946d470daf3e1a22
edwinQ2000 <51457661+edwinQ2000@users.noreply.github.com>
2020-06-26 20:33:00 -0500

    Add files via upload

--- ejercicios funcionales.py
+++ ejercicios funcionales.py
@@ -20,6 +20,22 @@ def p2(lista):
 
 def p3(lista):
     return [(int(max(str(y))),int(min(str(y))),int(max(str(y))) +int( min(str(y)))) for y in lista] 
+
+class Nodo():
+    def __init__(self,valor,izquierda=None,derecha=None):
+        self.valor=valor
+        self.izquierda=izquierda
+        self.derecha=derecha
+
+def en_orden(arbol):
+    if arbol ==None:
+        return []
+    return en_orden(arbol.izquierda) + [arbol.valor] + en_orden(arbol.derecha)
+
+def p4(lista):
+    return [[int(y)  for y in str(x)] for x in lista]
 print(p1([123,456,789]))
 print(p2([123,456]))
 print(p3([1234,5678]))
+arbol= Nodo(25,Nodo(15,None,Nodo(20)),Nodo(50))
+print(p4(en_orden(arbol)))
